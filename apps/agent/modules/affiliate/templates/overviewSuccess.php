3583e9324533a0d564c5ae578935e670fdba623f
khanmuhammad <khan.muhammad@zerocall.com>
2012-07-04 12:57:34 +0500

    new changes

--- apps/agent/modules/affiliate/templates/overviewSuccess.php
+++ apps/agent/modules/affiliate/templates/overviewSuccess.php
@@ -55,7 +55,7 @@
             ?>
                 <tr>
                     <td><strong><?php echo __('Your Balance is:') ?></strong></td>
-                    <td align="right"><?php echo $agent->getBalance(); ?></td>
+                    <td align="right"><?php echo number_format($agent->getBalance(),2); ?></td>
                 </tr>
             <?php } ?>
             <tr>
@@ -68,14 +68,14 @@
             <tr>
                 <td><?php echo __('Total') ?> <strong><?php echo __('revenue on registration') ?></strong></td>
                 <td align="right">
-                    <?php echo $registration_revenue
+                    <?php echo number_format($registration_revenue,2)
                     ?>
                 </td>
             </tr>
             <tr>
                 <td><?php echo __('Total commission earned on registration:') ?></td>
                 <td align="right">
-<?php echo $registration_commission;
+<?php echo number_format($registration_commission,2);
 ?>
                 </td>
             </tr>
@@ -86,14 +86,14 @@
             <tr>
                 <td><?php echo __('Total') ?> <strong><?php echo __('revenue on refill') ?></strong></td>
                 <td align="right">
-<?php echo $refill_revenue
+<?php echo number_format($refill_revenue,2)
 ?>
                 </td>
             </tr>
             <tr>
                 <td><?php echo __('Total commission earned on refill:') ?></td>
                 <td align="right">
-<?php echo $refill_com ?>
+<?php echo number_format($refill_com,2) ?>
                 </td>
             </tr>
 
@@ -103,13 +103,13 @@
             <tr>
                 <td><?php echo __('Total') ?> <strong><?php echo __('revenue earned') ?>  </strong><?php echo __('on refill from shop:') ?></td>
                 <td align="right">
-<?php echo $ef_sum; ?>
+<?php echo number_format($ef_sum,2); ?>
                 </td>
             </tr>
             <tr>
                 <td><?php echo __('Total') ?> <strong><?php echo __('commission earned') ?> </strong><?php echo __('on refill from shop:') ?></td>
                 <td align="right">
-                    <?php echo $ef_com; ?>
+                    <?php echo number_format($ef_com,2); ?>
                 </td>
             </tr>
             <tr>
@@ -118,13 +118,13 @@
        <!--       <tr>
                 <td><?php echo __('Total') ?> <strong>revenue </strong><?php echo __('on SMS Registeration:') ?></td>
                     <td align="right">
-<?php echo $sms_registration_earnings; ?>
+<?php echo number_format($sms_registration_earnings,2); ?>
 		</td>
 	</tr>
           <tr>
                     <td><?php echo __('Total') ?> <strong> <?php echo __('Commission earned') ?> </strong><?php echo __('on SMS Registeration:') ?></td>
                     <td align="right">
-<?php echo $sms_commission_earnings; ?>
+<?php echo number_format($sms_commission_earnings,2); ?>
                     </td>
             </tr>
             -->
