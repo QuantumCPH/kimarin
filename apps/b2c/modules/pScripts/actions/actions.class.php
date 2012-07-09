fe6eede4f4b5c55f6cd4053c7507c00cd9db07ef
Baran Khan <bk@zapna.com>
2012-07-09 11:37:49 +0500

    Tax for Refill

--- apps/b2c/modules/pScripts/actions/actions.class.php
+++ apps/b2c/modules/pScripts/actions/actions.class.php
@@ -2708,7 +2708,7 @@ if(($caltype!="IC") && ($caltype!="hc")){
 
              echo $unidc;
              echo "<br/>";
-
+             $OpeningBalance = $OpeningBalance/1.18;
             Telienta::recharge($this->customer, $OpeningBalance,'Refill');
             
             $getvoipInfo = new Criteria();
