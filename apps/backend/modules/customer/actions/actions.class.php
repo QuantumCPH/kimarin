c10bfb2d7bb7bb52ceadb78568a6145a655b2648
khanmuhammad <khan.muhammad@zerocall.com>
2012-07-10 13:15:09 +0500

    abc  test

--- apps/backend/modules/customer/actions/actions.class.php
+++ apps/backend/modules/customer/actions/actions.class.php
@@ -196,7 +196,7 @@ class customerActions extends autocustomerActions {
         $c = new Criteria();
         $c->add(CustomerPeer::ID, $id);
         $c->add(CustomerPeer::CUSTOMER_STATUS_ID, 3);
-        $c->addAnd(CustomerPeer::BLOCK,0);
+     
         $this->customer = CustomerPeer::doSelectOne($c);
 
 //$this->customer_balance =Telienta::getBalance($this->customer->getUniqueid());
