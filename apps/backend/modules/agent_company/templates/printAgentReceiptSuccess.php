d769421f99d643e8d7270b595493d647113f8c71
khanmuhammad <khan.muhammad@zerocall.com>
2012-07-13 13:00:22 +0500

    asdfghjkl changes means receipt changes

--- apps/backend/modules/agent_company/templates/printAgentReceiptSuccess.php
+++ apps/backend/modules/agent_company/templates/printAgentReceiptSuccess.php
@@ -112,7 +112,7 @@ $wrap_content  = isset($wrap)?$wrap:false;
   </tr>
   <tr class="footer">
     <td>&nbsp;</td>
-    <td><?php echo __('VAT') ?> (<?php echo '0%' ?>)</td>
+    <td><?php echo __('IVA') ?></td>
     <td>&nbsp;</td>
     <td align="right" style="padding-right:28px;"><?php echo format_number(0.00) ?><?php echo sfConfig::get('app_currency_code');?></td>
   </tr>
