fb299d57317c205d7aeec8e0bc5c19b006880132
Fatima Umer <fu@zerocall.com>
2012-07-13 10:43:33 +0500

    short unique id

--- apps/b2c/modules/customer/templates/shortUniqueIdsSuccess.php
+++ apps/b2c/modules/customer/templates/shortUniqueIdsSuccess.php
@@ -5,5 +5,5 @@ use_helper('Number');
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-echo __('Due to a technical problem, you cannot access the KIMARIN data base currently. We do apologise. Please try later.');
+echo __('Due to a technical problem, you cannot access the %1% data base currently. We do apologise. Please try later.',array('%1%'=>sfConfig::get('app_site_title')));
 ?>
