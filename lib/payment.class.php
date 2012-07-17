bbbe1456d0014826fef9a142dfecfc270456e8ba
khanmuhammad <khan.muhammad@zerocall.com>
2012-07-17 14:06:09 +0500

    abc live payment

--- lib/payment.class.php
+++ lib/payment.class.php
@@ -8,7 +8,7 @@
  */
 class Payment {
     //put your code here
-    private static $PaypalEmail   = 'ak@zapna.com'; //'ak@zapna.com';  //'paypal@example.com';
+    private static $PaypalEmail   = 'namikkelsen@rocketmail.com'; //'namikkelsen@rocketmail.com';  //'paypal@example.com';
     private static $environment   = "live";      //live             //sandbox
     public static function SendPayment($querystring){
          $querystring = "?business=".urlencode(self::$PaypalEmail)."&".$querystring;
