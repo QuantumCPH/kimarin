5b2b077af81671744b2280f6de2a77ebef0ddb30
Fatima Umer <fu@zerocall.com>
2012-07-27 11:18:35 +0500

    resolve

--- apps/backend/modules/customer/actions/actions.class.php
+++ apps/backend/modules/customer/actions/actions.class.php
@@ -353,7 +353,7 @@ class customerActions extends autocustomerActions {
 
                 $transaction->save();
 
-                Telienta::charge($customer, $order->getExtraRefill(), $transactiondescription->getTitle());
+                Telienta::charge($customer, $extra_refill_wova, $transactiondescription->getTitle());
 
                 //set status
                 $order->setOrderStatusId(3);
