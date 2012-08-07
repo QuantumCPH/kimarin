5c5639fe87c639294515410a18e0581fda77764c
Rubab Rafique <rr@zerocall.com>
2012-08-07 14:55:12 +0500

    local schema

---
+++ lib/filter/CallHistoryCallsLogFormFilter.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * CallHistoryCallsLog filter form.
+ *
+ * @package    zapnacrm
+ * @subpackage filter
+ * @author     Your name here
+ */
+class CallHistoryCallsLogFormFilter extends BaseCallHistoryCallsLogFormFilter
+{
+  public function configure()
+  {
+  }
+}
