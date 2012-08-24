93c54f64e0efe4e45b1a9299cf300b5b617806a1
Rubab Rafique <rr@zerocall.com>
2012-08-24 09:36:18 +0500

    change number email

--- lib/emailLib.php
+++ lib/emailLib.php
@@ -1764,6 +1764,103 @@ Uniuqe Id " . $uniqueid . " has issue while assigning on " . $customer->getMobil
         //-----------------------------------------
 
     }
+    
+    public static function sendCustomerChangeNumberEmail(Customer $customer, $order) {
+        $vat = 0;
+
+        $tc = new Criteria();
+        $tc->add(TransactionPeer::CUSTOMER_ID, $customer->getId());
+        $tc->addDescendingOrderByColumn(TransactionPeer::CREATED_AT);
+        $transaction = TransactionPeer::doSelectOne($tc);
+
+        //This Section For Get The Agent Information
+        $agent_company_id = $customer->getReferrerId();
+        if ($agent_company_id != '') {
+            $c = new Criteria();
+            $c->add(AgentCompanyPeer::ID, $agent_company_id);
+            $recepient_agent_email = AgentCompanyPeer::doSelectOne($c)->getEmail();
+            $recepient_agent_name = AgentCompanyPeer::doSelectOne($c)->getName();
+        } else {
+            $recepient_agent_email = '';
+            $recepient_agent_name = '';
+        }
+        //$this->renderPartial('affiliate/order_receipt', array(
+        sfContext::getInstance()->getConfiguration()->loadHelpers('Partial');
+        $message_body = get_partial('pScript/change_number_order_receipt', array(
+                    'customer' => $customer,
+                    'order' => $order,
+                    'transaction' => $transaction,
+                    'vat' => $vat,
+                    'agent_name' => $recepient_agent_name,
+                    'wrap' => false,
+                ));
+
+        $subject = __('Change Number Confirmation');
+        $recepient_email = trim($customer->getEmail());
+        $recepient_name = sprintf('%s %s', $customer->getFirstName(), $customer->getLastName());
+        $customer_id = trim($customer->getId());
+
+        //Support Information
+        $sender_name = sfConfig::get('app_email_sender_name');
+        $sender_email = sfConfig::get('app_email_sender_email');
+
+        $sender_namecdu = sfConfig::get('app_email_sender_name_cdu');
+        $sender_emailcdu = sfConfig::get('app_email_sender_email_cdu');
+        //------------------Sent The Email To Customer
+        if (trim($recepient_email) != '') {
+            $email = new EmailQueue();
+            $email->setSubject($subject);
+            $email->setReceipientName($recepient_name);
+            $email->setReceipientEmail($recepient_email);
+            $email->setAgentId($agent_company_id);
+            $email->setCutomerId($customer_id);
+            $email->setEmailType('Change Number ');
+            $email->setMessage($message_body);
+            $email->save();
+        }
+        //----------------------------------------
+        //------------------Sent the Email To Agent
+        if (trim($recepient_agent_email) != ''):
+
+            $email2 = new EmailQueue();
+            $email2->setSubject($subject);
+            $email2->setReceipientName($recepient_agent_name);
+            $email2->setReceipientEmail($recepient_agent_email);
+            $email2->setAgentId($agent_company_id);
+            $email2->setCutomerId($customer_id);
+            $email2->setEmailType('Change Number ');
+            $email2->setMessage($message_body);
+
+            $email2->save();
+        endif;
+        //---------------------------------------
+        //--------------Sent The Email To okhan
+        if (trim($sender_email) != ''):
+            $email3 = new EmailQueue();
+            $email3->setSubject($subject);
+            $email3->setReceipientName($sender_name);
+            $email3->setReceipientEmail($sender_email);
+            $email3->setAgentId($agent_company_id);
+            $email3->setCutomerId($customer_id);
+            $email3->setEmailType('Change Number ');
+            $email3->setMessage($message_body);
+            $email3->save();
+        endif;
+        //-----------------------------------------
+        //--------------Sent The Email To cdu
+        if (trim($sender_emailcdu) != ''):
+            $email4 = new EmailQueue();
+            $email4->setSubject($subject);
+            $email4->setReceipientName($sender_namecdu);
+            $email4->setReceipientEmail($sender_emailcdu);
+            $email4->setAgentId($agent_company_id);
+            $email4->setCutomerId($customer_id);
+            $email4->setEmailType('Change Number ');
+            $email4->setMessage($message_body);
+            $email4->save();
+        endif;
+        //-----------------------------------------
+    }
 
 }
 
