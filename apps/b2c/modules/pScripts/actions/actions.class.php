75b3a55cc732116dd158837eb05f02ae672b9418
khanmuhammad <khan.muhammad@zerocall.com>
2012-08-24 11:42:51 +0500

    admin site changes

--- apps/b2c/modules/pScripts/actions/actions.class.php
+++ apps/b2c/modules/pScripts/actions/actions.class.php
@@ -3297,9 +3297,9 @@ public function executeSmsRegisterationwcb(sfWebrequest $request) {
 //        $this->todate = $fromdate;
             
               $fromdate = mktime(0, 0, 0, date("m")-1, date("d") - 1, date("Y"));
-        $this->fromdate = date("Y-m-d", $fromdate);
+    $this->fromdate = date("Y-m-d", $fromdate);
           $todate = mktime(0, 0, 0, date("m"), date("d"), date("Y"));
-        $this->todate = $todate;
+         $this->todate =date("Y-m-d", $todate);
 
        $tilentaCallHistryResult = Telienta::callHistory($customer, $this->fromdate . ' 00:00:00', $this->todate . ' 23:59:59');
     var_dump($tilentaCallHistryResult);
