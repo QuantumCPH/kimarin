c5c484ceffcf1a7268e5d052ba3b804b1e4769cc
khanmuhammad <khan.muhammad@zerocall.com>
2012-08-27 13:38:48 +0500

    change product change

--- lib/form/base/BaseCustomerChangeProductForm.class.php
+++ lib/form/base/BaseCustomerChangeProductForm.class.php
@@ -12,21 +12,25 @@ class BaseCustomerChangeProductForm extends BaseFormPropel
   public function setup()
   {
     $this->setWidgets(array(
-      'id'           => new sfWidgetFormInputHidden(),
-      'customer_id'  => new sfWidgetFormInput(),
-      'product_id'   => new sfWidgetFormInput(),
-      'created_at'   => new sfWidgetFormDateTime(),
-      'status'       => new sfWidgetFormInput(),
-      'execuated_at' => new sfWidgetFormDateTime(),
+      'id'             => new sfWidgetFormInputHidden(),
+      'customer_id'    => new sfWidgetFormInput(),
+      'product_id'     => new sfWidgetFormInput(),
+      'created_at'     => new sfWidgetFormDateTime(),
+      'status'         => new sfWidgetFormInput(),
+      'execuated_at'   => new sfWidgetFormDateTime(),
+      'order_id'       => new sfWidgetFormInput(),
+      'transaction_id' => new sfWidgetFormInput(),
     ));
 
     $this->setValidators(array(
-      'id'           => new sfValidatorPropelChoice(array('model' => 'CustomerChangeProduct', 'column' => 'id', 'required' => false)),
-      'customer_id'  => new sfValidatorInteger(),
-      'product_id'   => new sfValidatorInteger(),
-      'created_at'   => new sfValidatorDateTime(),
-      'status'       => new sfValidatorInteger(),
-      'execuated_at' => new sfValidatorDateTime(),
+      'id'             => new sfValidatorPropelChoice(array('model' => 'CustomerChangeProduct', 'column' => 'id', 'required' => false)),
+      'customer_id'    => new sfValidatorInteger(),
+      'product_id'     => new sfValidatorInteger(),
+      'created_at'     => new sfValidatorDateTime(),
+      'status'         => new sfValidatorInteger(),
+      'execuated_at'   => new sfValidatorDateTime(),
+      'order_id'       => new sfValidatorInteger(array('required' => false)),
+      'transaction_id' => new sfValidatorInteger(array('required' => false)),
     ));
 
     $this->widgetSchema->setNameFormat('customer_change_product[%s]');
