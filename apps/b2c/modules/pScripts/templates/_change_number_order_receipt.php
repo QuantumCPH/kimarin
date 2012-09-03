b43df261e17f17e4c8d45e395e99ffc2c26f4a64
Fatima Umer <fu@zerocall.com>
2012-09-03 16:02:55 +0500

    change number

--- apps/b2c/modules/pScripts/templates/_change_number_order_receipt.php
+++ apps/b2c/modules/pScripts/templates/_change_number_order_receipt.php
@@ -2,121 +2,77 @@
 use_helper('I18N');
 use_helper('Number');
 ?>
-<style>
-	p {
-		margin: 8px auto;
-	}
-
-	table.receipt {
-		width: 600px;
-		
-		border: 2px solid #ccc;
-	}
-
-	table.receipt td, table.receipt th {
-		padding:5px;
-                font-size:14px;
-	}
-
-	table.receipt th {
-		text-align: left;
-	}
-
-	table.receipt .payer_details {
-		padding: 10px 0;
-	}
-
-	table.receipt .receipt_header, table.receipt .order_summary_header {
-		font-weight: bold;
-		text-transform: uppercase;
-	}
-
-	table.receipt .footer
-	{
-		font-weight: bold;
-	}
-
-
-</style>
-
-
 <table width="600px">
-	<tr style="border:0px solid #fff">
-		<td colspan="4" align="right" style="text-align:right; border:0px solid #fff"><?php echo image_tag(sfConfig::get('app_web_url').'images/logo.png',array('width' => '170'));?></td>
-	</tr>
+    <tr style="border:0px solid #fff">
+        <td colspan="4" align="right" style="text-align:right; border:0px solid #fff"><?php echo image_tag(sfConfig::get('app_web_url').'images/logo.png',array('width' => '170'));?></td>
+    </tr>
 </table>
-<table class="receipt" cellspacing="0" width="600px">
-  <tr bgcolor="#CCCCCC" class="receipt_header">
-    <th colspan="3"><?php echo __('Order Receipt') ?></th>
+<table cellspacing="0" width="600px" style="border: 2px solid #ccc;">
+    <tr bgcolor="#CCCCCC" style="font-weight: bold;text-transform: uppercase;">
+            <th colspan="3" align="left"><?php echo __('Order Receipt') ?></th>
     <th><?php echo __('Order No') ?> <?php echo $order->getId() ?></th>
-  </tr>
-
-  <tr>
-    <td colspan="4" class="payer_summary">
-      <?php echo __('Customer Number') ?>   <?php echo $customer->getUniqueId(); ?><br/>
-      <?php echo sprintf("%s %s", $customer->getFirstName(), $customer->getLastName())?><br/>
-      <?php echo $customer->getAddress() ?><br/>
-      <?php echo sprintf('%s %s', $customer->getPoBoxNumber(),$customer->getCity() ) ?><br/>
-      <?php
-	  $eC = new Criteria();
-	  $eC->add(EnableCountryPeer::ID, $customer->getCountryId());
-	  $eC = EnableCountryPeer::doSelectOne($eC);
-	 // echo $eC->getName();
-	  ?>
-
-
-      <br /><br />
-      <?php echo __('Mobile Number') ?>: <br />
-      <?php echo $customer->getMobileNumber() ?><br />
-
-      <?php if($agent_name!=''){ echo __('Agent Name') ?>:  <?php echo $agent_name; } ?>
-    </td>
-  </tr>
-  <tr class="order_summary_header" bgcolor="#CCCCCC">
-    <td><?php echo __('Date') ?></td>
-    <td><?php echo __('Description') ?></td>
-    <td><?php echo __('Quantity') ?></td>
-  <td align="right" style="padding-right: 65px;"><?php echo __('Amount') ?><!--    (<?php //echo sfConfig::get('app_currency_code');?>)--></td>
-  </tr>
-  <tr>
-    <td><?php echo $order->getCreatedAt('d-m-Y') ?></td>
-    <td><?php echo __($transaction->getDescription());?></td>
-    <td><?php echo $order->getQuantity() ?></td>
-    <td align="right" style="padding-right: 65px;"><?php echo number_format($subtotal = $transaction->getAmount()-$vat,2); ?><?php echo sfConfig::get('app_currency_code');?></td>
-  </tr>
-  <tr>
-  	<td colspan="4" style="border-bottom: 2px solid #c0c0c0;">&nbsp;</td>
-  </tr>
-  <tr class="footer">
-    <td>&nbsp;</td>
-    <td><?php echo __('Subtotal') ?></td>
-    <td>&nbsp;</td>
-    <td align="right" style="padding-right: 65px;"><?php echo number_format($subtotal,2);  ?><?php echo sfConfig::get('app_currency_code');?></td>
-  </tr>
-
-  <tr class="footer">
-    <td>&nbsp;</td>
-   <td><?php echo __('IVA') ?> <!-- (<?php //echo $vat==0?'0%':sfConfig::get('app_vat') ?>)--></td>
-    <td>&nbsp;</td>
-    <td align="right" style="padding-right: 65px;"><?php echo number_format($vat,2); ?><?php echo sfConfig::get('app_currency_code');?></td>
-  </tr>
-  <tr class="footer">
-    <td>&nbsp;</td>
-    <td><?php echo __('Total') ?></td>
-    <td>&nbsp;</td>
-    <td align="right" style="padding-right: 65px;"><?php echo number_format(($subtotal+$vat),2); ?><?php echo sfConfig::get('app_currency_code');?></td>
-  </tr>
-  <tr>
-  	<td colspan="4" style="border-bottom: 2px solid #c0c0c0;">&nbsp;</td>
-  </tr>
-  <tr class="footer">
-    <td class="payer_summary" colspan="4" style="font-weight:normal; white-space: nowrap;">
-    <?php echo __('%1%',array('%1%'=>sfConfig::get('app_postal_address_bottom')));?> </td>
-  </tr>
+    </tr>
+    <tr>
+        <td colspan="4">
+          <?php echo __('Customer Number') ?>   <?php echo $customer->getUniqueId(); ?><br/>
+          <?php echo sprintf("%s %s", $customer->getFirstName(), $customer->getLastName())?><br/>
+          <?php echo $customer->getAddress() ?><br/>
+          <?php echo sprintf('%s %s', $customer->getPoBoxNumber(),$customer->getCity() ) ?><br/>
+          <?php
+          $eC = new Criteria();
+          $eC->add(EnableCountryPeer::ID, $customer->getCountryId());
+          $eC = EnableCountryPeer::doSelectOne($eC);
+         // echo $eC->getName();
+          ?>
+          <br /><br />
+          <?php echo __('Mobile Number') ?>: <br />
+          <?php echo $customer->getMobileNumber() ?><br />
+          <?php if($agent_name!=''){ echo __('Agent Name') ?>:  <?php echo $agent_name; } ?>
+        </td>
+    </tr>
+    <tr bgcolor="#CCCCCC" style="font-weight: bold;text-transform: uppercase;">
+        <td><?php echo __('Date') ?></td>
+        <td><?php echo __('Description') ?></td>
+        <td><?php echo __('Quantity') ?></td>
+        <td align="right" style="padding-right: 65px;"><?php echo __('Amount') ?></td>
+    </tr>
+    <tr>
+        <td><?php echo $order->getCreatedAt('d-m-Y') ?></td>
+        <td><?php echo __($transaction->getDescription());?></td>
+        <td><?php echo $order->getQuantity() ?></td>
+        <td align="right" style="padding-right: 65px;"><?php echo number_format($subtotal = $transaction->getAmount()-$vat,2); ?><?php echo sfConfig::get('app_currency_code');?></td>
+    </tr>
+    <tr>
+        <td colspan="4" style="border-bottom: 2px solid #c0c0c0;">&nbsp;</td>
+    </tr>
+    <tr class="footer">
+        <td>&nbsp;</td>
+        <td><?php echo __('Subtotal') ?></td>
+        <td>&nbsp;</td>
+        <td align="right" style="padding-right: 65px;"><?php echo number_format($subtotal,2);  ?><?php echo sfConfig::get('app_currency_code');?></td>
+    </tr>
+    <tr class="footer">
+        <td>&nbsp;</td>
+        <td><?php echo __('IVA') ?></td>
+        <td>&nbsp;</td>
+        <td align="right" style="padding-right: 65px;"><?php echo number_format($vat,2); ?><?php echo sfConfig::get('app_currency_code');?></td>
+    </tr>
+    <tr class="footer">
+        <td>&nbsp;</td>
+        <td><?php echo __('Total') ?></td>
+        <td>&nbsp;</td>
+        <td align="right" style="padding-right: 65px;"><?php echo number_format(($subtotal+$vat),2); ?><?php echo sfConfig::get('app_currency_code');?></td>
+    </tr>
+    <tr>
+        <td colspan="4" style="border-bottom: 2px solid #c0c0c0;">&nbsp;</td>
+    </tr>
+    <tr>
+        <td colspan="4" style="font-weight:normal; white-space: nowrap;"><?php echo __('%1%',array('%1%'=>sfConfig::get('app_postal_address_bottom')));?> </td>
+    </tr>
 </table>
 <p style="font-weight: bold;">
-	<?php echo __('If you have any inquiries please contact %1% Customer Support.',array('%1%' => sfConfig::get('app_site_title'))); ?>
-        <br><?php echo __('E-mail') ?>:&nbsp;
-	<a href="mailto:<?php echo sfConfig::get('app_support_email_id');?>"><?php echo sfConfig::get('app_support_email_id');?></a>
-        <br><?php echo __('Telephone') ?>:&nbsp;<?php echo sfConfig::get('app_phone_no');?>
+    <?php echo __('If you have any inquiries please contact %1% Customer Support.',array('%1%' => sfConfig::get('app_site_title'))); ?>
+    <br><?php echo __('E-mail') ?>:&nbsp;
+    <a href="mailto:<?php echo sfConfig::get('app_support_email_id');?>"><?php echo sfConfig::get('app_support_email_id');?></a>
+    <br><?php echo __('Telephone') ?>:&nbsp;<?php echo sfConfig::get('app_phone_no');?>
 </p>
