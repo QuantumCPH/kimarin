932402da8b281c6908034d1a4bf4f0321fdda5b8
Fatima Umer <fu@zerocall.com>
2012-09-03 17:20:14 +0500

    change product

--- apps/b2c/modules/pScripts/templates/_change_number_order_receipt.php
+++ apps/b2c/modules/pScripts/templates/_change_number_order_receipt.php
@@ -9,8 +9,8 @@ use_helper('Number');
 </table>
 <table cellspacing="0" width="600px" style="border: 2px solid #ccc;">
     <tr bgcolor="#CCCCCC" style="font-weight: bold;text-transform: uppercase;">
-            <th colspan="3" align="left"><?php echo __('Order Receipt') ?></th>
-    <th><?php echo __('Order number') ?>: <?php echo $order->getId() ?></th>
+        <th colspan="3" align="left"><?php echo __('Order Receipt') ?></th>
+        <th><?php echo __('Order number') ?>: <?php echo $order->getId() ?></th>
     </tr>
     <tr>
         <td colspan="4">
@@ -45,19 +45,19 @@ use_helper('Number');
     <tr>
         <td colspan="4" style="border-bottom: 2px solid #c0c0c0;">&nbsp;</td>
     </tr>
-    <tr class="footer">
+    <tr>
         <td>&nbsp;</td>
         <td><?php echo __('Subtotal') ?></td>
         <td>&nbsp;</td>
         <td align="right" style="padding-right: 65px;"><?php echo number_format($subtotal,2);  ?><?php echo sfConfig::get('app_currency_code');?></td>
     </tr>
-    <tr class="footer">
+    <tr>
         <td>&nbsp;</td>
         <td><?php echo __('IVA') ?></td>
         <td>&nbsp;</td>
         <td align="right" style="padding-right: 65px;"><?php echo number_format($vat,2); ?><?php echo sfConfig::get('app_currency_code');?></td>
     </tr>
-    <tr class="footer">
+    <tr>
         <td>&nbsp;</td>
         <td><?php echo __('Total') ?></td>
         <td>&nbsp;</td>
