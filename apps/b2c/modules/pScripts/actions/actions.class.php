8f2ba0086f70508e1ab2102962e1ce06f9dc036c
Rubab Rafique <rr@zerocall.com>
2012-09-04 15:00:52 +0500

    bonus email

--- apps/b2c/modules/pScripts/actions/actions.class.php
+++ apps/b2c/modules/pScripts/actions/actions.class.php
@@ -3635,7 +3635,7 @@ public function executeSmsRegisterationwcb(sfWebrequest $request) {
   }
  
    public function executeChangeCustomerProduct(sfWebRequest $request){
-   //if(date("d")!=1){die;}
+   if(date("d")!=1){die;}
             $ccp = new Criteria();
             $ccp->add(CustomerChangeProductPeer::STATUS, 2);
             $ChangeCustomers=CustomerChangeProductPeer::doSelect($ccp);
