27cc250a9b7be276a8f8009f4d512e61433dbe79
Rubab Rafique <rr@zerocall.com>
2012-09-04 14:49:22 +0500

    bonus email

--- apps/b2c/modules/payments/templates/_order_receipt_product_change.php
+++ apps/b2c/modules/payments/templates/_order_receipt_product_change.php
@@ -50,46 +50,33 @@ $wrap_content  = isset($wrap)?$wrap:false;
 
 <table width="600px">
 <tr style="border:0px solid #fff">
-
-		<td colspan="4" align="right" style="text-align:right; border:0px solid #fff"><?php echo image_tag(sfConfig::get('app_web_url').'images/logo.png',array('width' => '170'));?></td>
-
-	</tr>
+   <td colspan="4" align="right" style="text-align:right; border:0px solid #fff"><?php echo image_tag(sfConfig::get('app_web_url').'images/logo.png',array('width' => '170'));?></td>
+</tr>
 </table>
 <table class="receipt" cellspacing="0" width="600px">
-	
     <?php 
 	  $eC = new Criteria();
 	  $eC->add(CustomerProductPeer::CUSTOMER_ID, $customer->getId());
           $eC->addAnd(CustomerProductPeer::STATUS_ID,3);
 	  $cProduct = CustomerProductPeer::doSelectOne($eC);
           $product=  ProductPeer::retrieveByPK($cProduct->getProductId());
-	
-          ?> 
-   
-   <tr>
-  	<td colspan="4"  > 
-            <?php echo __('Dear Customer'); ?>   <?php echo sprintf("%s %s", $customer->getFirstName(), $customer->getLastName())?>  ,<br/>
+?> 
+  <tr>
+  	<td colspan="4" style='font-family:"Times New Roman", Times, serif;font-size: 14px;'> 
+            <?php echo __('Dear Customer'); ?>   <?php //echo sprintf("%s %s", $customer->getFirstName(), $customer->getLastName())?>  ,<br/>
  
    <?php echo __('Your order for change of product has been successfully implemented');  ?>.<br/>
- <?php echo __('Now your current product is'); ?> ( <?php echo $product->getName();   ?>).
+ <?php echo __('Now your current product is'); ?>: (<?php echo $product->getName();   ?>).
   </td>
   </tr>
-  
-  
   <tr>
   	<td colspan="4" style="border-bottom: 2px solid #c0c0c0;">&nbsp;</td>
   </tr>
   <tr class="footer">
-    <td class="payer_summary" colspan="4" style="font-weight:normal; white-space: nowrap;"> 
+    <td class="payer_summary" colspan="4" style='font-weight:normal; white-space: nowrap;font-family:"Times New Roman", Times, serif;font-size: 14px;'> 
     <?php echo __('%1%',array('%1%'=>sfConfig::get('app_postal_address_bottom')))?> </td>
   </tr>
 </table>
-
-
-
-
-
-
 <?php if($wrap_content): ?>
 <br />
 <p>
@@ -108,19 +95,10 @@ $wrap_content  = isset($wrap)?$wrap:false;
 	<?php echo __('You will receive your package within %1%.', array('%1%'=>$expected_delivery)) ?> 
 </p>
 <?php endif; ?>
-
-<p style="font-weight: bold;">
+<p><br /></p>
+<p style='font-weight: bold;font-family:"Times New Roman", Times, serif;font-size: 14px;'>
 	<?php echo __('If you have any inquiries please contact %1% Customer Support.',array('%1%' => sfConfig::get('app_site_title'))); ?>
         <br><?php echo __('E-mail') ?>:&nbsp;
 	<a href="mailto:<?php echo sfConfig::get('app_support_email_id');?>"><?php echo sfConfig::get('app_support_email_id');?></a>
         <br><?php echo __('Telephone') ?>:&nbsp;<?php echo sfConfig::get('app_phone_no');?>
 </p>
-
-<!--<p style="font-weight: bold;"><?php echo __('Cheers') ?></p>
-
-<p style="font-weight: bold;">
-
-<?php echo __('%1% Support',array('%1%'=>sfConfig::get('app_site_title'))) ?>&nbsp;
-
-</p>-->
-
