90c8398ce7b9f6579b57c1a91637c6680487251a
khanmuhammad <khan.muhammad@zerocall.com>
2012-09-05 13:03:05 +0500

    abcd block customer detail

--- apps/b2c/modules/customer/templates/_block_customer.php
+++ apps/b2c/modules/customer/templates/_block_customer.php
@@ -59,21 +59,14 @@ $wrap_content  = isset($wrap)?$wrap:false;
 <p><?php echo __('Dear Customer') ?></p>
    <p><?php echo __('Your account has been blocked.') ?></p>
          <p><?php echo __('Your customer number is') ?>: <?php   echo $customer->getUniqueId();   ?></p>
-<p>
-</p>
-<p style="font-weight: bold;">
-	<?php echo __('Please contact Kimarin Customer Support to re-open your account.') ?> 
-</p>
- 
 
-<p style="font-weight: bold;">
+<p style="font-weight: bold;"><?php echo __('Please contact Kimarin Customer Support to re-open your account.') ?></p>
+ <p style="font-weight: bold;">
 	<?php echo __('If you have any inquiries please contact %1% Customer Support.',array('%1%' => sfConfig::get('app_site_title'))); ?>
         <br><?php echo __('E-mail') ?>:&nbsp;
 	<a href="mailto:<?php echo sfConfig::get('app_support_email_id');?>"><?php echo sfConfig::get('app_support_email_id');?></a>
         <br><?php echo __('Telephone') ?>:&nbsp;<?php echo sfConfig::get('app_phone_no');?>
 </p>
-
- <p style="font-weight: bold;"><?php echo __('Best regards') ?>,</p>
-
-<p style="font-weight: bold;"><?php echo  sfConfig::get('app_site_title'); ?>&nbsp;</p> 
+ <p style="font-weight: bold;"><?php echo __('Best regards'); ?>,</p>
+<p style="font-weight: bold;"><?php echo  sfConfig::get('app_site_title'); ?></p> 
 
