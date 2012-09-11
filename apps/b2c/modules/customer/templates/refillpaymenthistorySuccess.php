9a0be986ff61866065c40d18d4b53fb17b0e1acf
khanmuhammad <khan.muhammad@zerocall.com>
2012-09-11 15:31:10 +0500

    url changes

--- apps/b2c/modules/customer/templates/refillpaymenthistorySuccess.php
+++ apps/b2c/modules/customer/templates/refillpaymenthistorySuccess.php
@@ -80,7 +80,7 @@
                   <td>
                       
                       
-                      <a href="#" class="receipt"   onclick="javascript: window.open('<?php echo url_for('payments/showReceipt?tid='.$transaction->getId(), true) ?>')"  >
+                      <a href="#" class="receipt"  target="_parent"  onclick="javascript: window.open('<?php echo url_for('payments/showReceipt?tid='.$transaction->getId(), true) ?>')"  >
                             <?php //echo $tdescription;
                               if(strstr($tdescription, "bonus")){
                                 echo __('Bonus');
