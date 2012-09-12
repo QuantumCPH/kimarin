e45d3dd62a8a3b2fb8a0b3163a8698c570bbf487
Baran Khan <bk@zapna.com>
2012-09-12 13:19:21 +0500

    web sms 0 balance ifixation

--- apps/b2c/modules/customer/templates/websmsSuccess.php
+++ apps/b2c/modules/customer/templates/websmsSuccess.php
@@ -59,11 +59,8 @@ if ($msgSent != '') {
 
 
 <?php
-echo "<h1>";
-var_dump($balance);
-echo "</h1>";
 
- if ($balance>0) { ?>
+ if (number_format($balance, 2)>0) { ?>
             <form action=<?php echo url_for('customer/websms', true) ?>  method="post" id="websms" onsubmit="isHex(this.value)">
                 <h3 style="width: 400px;"><?php echo __("Your account balance is") ?>:<?php echo number_format($balance, 2); ?><?php echo sfConfig::get('app_currency_code') ?></h3>
 <?php } else { ?>
