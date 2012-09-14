8357f3a642c3cf2dfee35803c719c2443b07ae65
Rubab Rafique <rr@zerocall.com>
2012-09-14 13:06:56 +0500

    payment

--- apps/b2c/modules/payments/actions/actions.class.php
+++ apps/b2c/modules/payments/actions/actions.class.php
@@ -272,7 +272,7 @@ class paymentsActions extends sfActions {
         $customer_order = CustomerOrderPeer::retrieveByPK($transaction->getOrderId());
        // $this->customer_order = $customer_order;
         $customerorder = $customer_order->getIsFirstOrder();
-        if ($customerorder) {
+        if ($customerorder==1) {
             if($transaction_id>489){
             $vat = ($customer_order->getProduct()->getRegistrationFee()+$postalcharge) * sfConfig::get('app_vat_percentage');
             }else{
