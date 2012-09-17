8985d9e512ee9c237efaa0faa1469dcf5bf4162f
Rubab Rafique <rr@zerocall.com>
2012-09-17 16:21:36 +0500

    isfirstorder

--- apps/b2c/modules/payments/templates/_order_receipt_us.php
+++ apps/b2c/modules/payments/templates/_order_receipt_us.php
@@ -102,7 +102,7 @@ $wrap_content  = isset($wrap)?$wrap:false;
   <tr> 
     <td><?php echo $order->getCreatedAt('m-d-Y') ?></td>
     <td>
-    <?php if ($order->getIsFirstOrder())
+    <?php if ($order->getIsFirstOrder()==1)
     {
         echo $order->getProduct()->getName();
         echo  '<br />['. __($transaction->getDescription()) .']';
