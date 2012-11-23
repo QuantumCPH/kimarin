c5c046088968e46f8c5039884fd809c397ee38ad
khanmuhammad <khan.muhammad@zerocall.com>
2012-11-23 11:40:46 +0500

    changes for 11/23/2012

--- apps/backend/modules/agent_company/templates/_agent_order_receipt.php
+++ apps/backend/modules/agent_company/templates/_agent_order_receipt.php
@@ -61,12 +61,12 @@ $wrap_content  = isset($wrap)?$wrap:false;
 	</p>
 	<br />
 <?php endif; ?>
-         <table cellspacing="0" width="600px" style="border: 2px solid #ccc;">
+         <table width="600px" >
 <tr style="border:0px solid #fff;">
 <td colspan="4" align="right" style="text-align:right; border:0px solid #fff"><?php echo image_tag(sfConfig::get('app_web_url').'images/logo.png',array('width' => '170'));?></td>
 	</tr>
 </table>
-<table class="receipt" cellspacing="0" width="600px">
+<table class="receipt" cellspacing="0" width="600px" style="border: 2px solid #ccc;">
    <tr bgcolor="#CCCCCC" class="receipt_header"> 
     <th colspan="3"  align="left" ><?php echo __('Order Receipt') ?></th>
     <th><?php echo __('Order No.') ?> <?php echo $order; ?></th>
