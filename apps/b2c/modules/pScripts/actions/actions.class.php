b06870183a943b94b485324e8272a06d57daa25c
khanmuhammad <khan.muhammad@zerocall.com>
2013-04-22 10:46:14 +0500

    account fetchning

--- apps/b2c/modules/pScripts/actions/actions.class.php
+++ apps/b2c/modules/pScripts/actions/actions.class.php
@@ -4682,8 +4682,8 @@ class pScriptsActions extends sfActions {
          
           $c = new Criteria;
           $c->add(CustomerPeer::CUSTOMER_STATUS_ID,3);
-           $c->addAnd(CustomerPeer::ID,61,Criteria::GREATER_EQUAL);
-          $c->addAnd(CustomerPeer::ID,90,Criteria::LESS_EQUAL);
+           $c->addAnd(CustomerPeer::ID,91,Criteria::GREATER_EQUAL);
+          $c->addAnd(CustomerPeer::ID,120,Criteria::LESS_EQUAL);
           $customers=CustomerPeer::doSelect($c);
    foreach($customers as $customer){
               
