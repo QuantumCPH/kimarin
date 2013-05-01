5c6056d40aae5797db6de913901d289a506a02b2
Rubab Rafique <rr@zerocall.com>
2013-05-01 11:28:52 +0500

    get os of customer

--- apps/b2c/modules/customer/templates/appThanksSuccess.php
+++ apps/b2c/modules/customer/templates/appThanksSuccess.php
@@ -2,9 +2,22 @@
 use_helper('I18N');
 ?>
 <div class="">
+    <?php
+    $os = $_SERVER['HTTP_USER_AGENT'];    
+    if(strpos($os, "Android")!== false || strpos($os, "Iphone") !==false){ ?>
     <p>
         Descargar su Kimarin APP / download your Kimarin APP:<br />
         iPhone: xxxxxx<br />
         Android: yyyy<br />
     </p>
+    <?php
+    }else{
+    ?>
+    <p>
+        <strong>Download Instructions</strong>
+    </p>
+    <?php
+    }
+    ?>
+    
 </div><br clear="all" />
